6592fb5439595bb93171197dadb47346d11ac8eb
fran0770 <franmowat33@gmail.com>
2023-08-23 08:42:21 +0100

    operator function

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ let add = (a, b) => {return a+b};
 let subtract = (a, b) => {return a-b};
 let multiply = (a, b) => {return a*b};
 let divide = (a, b) => {return a/b};
+
+let value1;
+let value2;
+let operator;
+
+let operate = (value1, value2, operator) => {
+    switch (operator){
+        case "+":
+            add(value1, value2);
+            break;
+        case "-":
+            subtract(value1, value2);
+            break;
+        case "*":
+            multiply(value1, value2);
+            break;
+        case "/":
+            divide(value1, value2);
+            break;
+        default:
+            console.log("error with operator function");
+            break;
+    }
+}
